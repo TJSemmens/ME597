3595621a6c96740e23fa3f874ea132b85aa0e99e
turtlebot7 <aehassou@uwaterloo.ca>
2024-02-29 03:17:26 -0500

    labTwo first commit, couldnt valdiate Q4 or Q5 because couldn't get localization node to start

--- utilities.py
+++ utilities.py
@@ -1,12 +1,15 @@
-from math import atan2, asin, sqrt
+from math import atan2, asin, sqrt, pi
 
 M_PI=3.1415926535
 
 class Logger:
+    
     def __init__(self, filename, headers=["e", "e_dot", "e_int", "stamp"]):
+        
         self.filename = filename
 
         with open(self.filename, 'w') as file:
+            
             header_str=""
 
             for header in headers:
@@ -21,13 +24,11 @@ class Logger:
     def log_values(self, values_list):
 
         with open(self.filename, 'a') as file:
+            
             vals_str=""
-
-            # TODO Part 5: Write the values from the list to the file
-
-            for val in values_list:
-                vals_str += str(val)
-                vals_str += ", "
+            
+            for value in values_list:
+                vals_str+=f"{value}, "
             
             vals_str+="\n"
             
@@ -50,11 +51,11 @@ class FileReader:
         table=[]
         headers=[]
         with open(self.filename, 'r') as file:
-            # Skip the header line
 
             if not read_headers:
                 for line in file:
                     values=line.strip().split(',')
+
                     for val in values:
                         if val=='':
                             break
@@ -62,39 +63,69 @@ class FileReader:
 
                     read_headers=True
                     break
+            
             next(file)
+            
             # Read each line and extract values
             for line in file:
                 values = line.strip().split(',')
-
+                
                 row=[]                
+                
                 for val in values:
-                    replacers = {"[":"", "]" : "", ")":""}
-                    for symbol, space in replacers.items():
-                        val = val.replace(symbol, space)
                     if val=='':
                         break
-                    
-                    try:
-                        if val == " inf":
-                            # row.append(0)
-                            continue
-                        else:
-                            row.append(float(val.strip()))
-                    except:
-                        continue
+                    row.append(float(val.strip()))
+
                 table.append(row)
         
         return headers, table
+    
+    
 
-
-# TODO Part 5: Implement the conversion from Quaternion to Euler Angles
+# TODO Part 3: Implement the conversion from Quaternion to Euler Angles
 def euler_from_quaternion(quat):
     """
     Convert quaternion (w in last place) to euler roll, pitch, yaw.
     quat = [x, y, z, w]
     """
-    yaw = atan2(2*(quat.w*quat.z+quat.x*quat.y), 1 - 2*(quat.y**2 + quat.z**2))
+    siny_cosp = 2*(quat.w*quat.z+quat.x*quat.y)
+    cosy_cosp = 1 - 2*(quat.y*quat.y + quat.z*quat.z)
+    yaw = atan2(siny_cosp, cosy_cosp)
+    # just unpack yaw
     return yaw
 
 
+#TODO Part 4: Implement the calculation of the linear error
+def calculate_linear_error(current_pose, goal_pose):
+    print(current_pose)    
+    # Compute the linear error in x and y
+    # Remember that current_pose = [x,y, theta, time stamp] and goal_pose = [x,y,theta]
+    # Remember to use the Euclidean distance to calculate the error.
+    x_current, y_current = current_pose[:2]
+    x_goal, y_goal = goal_pose[:2]
+
+    error_linear= ((x_goal - x_current) ** 2 + (y_goal - y_current) ** 2) ** 0.5
+    
+    return error_linear
+
+#TODO Part 4: Implement the calculation of the angular error
+def calculate_angular_error(current_pose, goal_pose):
+
+    # Compute the linear error in x and y
+    # Remember that current_pose = [x,y, theta, time stamp] and goal_pose = [x,y,theta]
+    # Remember that this function returns the difference in orientation between where the robot currently faces and where it should face to reach the goal
+    
+    current_x, current_y = current_pose[:2]
+    goal_x, goal_y = goal_pose[:2]
+    error_angular = atan2(goal_y - current_y, goal_x - current_x) - current_pose[2]
+    # error_angular = goal_pose[2] - current_pose[2]
+    print(error_angular)
+    error_angular = (error_angular + pi) % (2 * pi) - pi #I'm taking the modulo of 0 to 2*pi but shifting it by pi in either direction so its -pi to pi
+
+
+    # Remember to handle the cases where the angular error might exceed the range [-π, π]
+
+    ...
+    
+    return error_angular
